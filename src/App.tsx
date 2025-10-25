92382007c8289f4f99e508ffbddd75625dbb5e8b
Yumare1234 <yumaregabriel@gmail.com>
2025-10-25 12:00:30 -0400

    actualizacion de mi proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import './App.css'
-
+import Carta from './components/Carta'
 function App() {
+  const mostrarCarta = (numero: number, pinta: string) => {
+    alert(`Carta de numero: ${numero} y de pinta ${pinta}` );
+  };
 
   return (
     <div>
       <h1> Mi Primer Proyecto en React </h1>
       <p> Nombre: Gabriel Yumare </p>
-      <p> Hola </p>
+       <Carta numero = {2} pinta = {"Diamante"} mostrarCarta={mostrarCarta}/>
+       <Carta numero = {3} pinta = {"Trebol"} mostrarCarta={mostrarCarta}/>
+       <Carta numero = {6} pinta = {"Corazon"} mostrarCarta={mostrarCarta}/>
+       <Carta numero = {18} pinta = {"Trebol"} mostrarCarta={mostrarCarta}/>
     </div>
   )
 }
