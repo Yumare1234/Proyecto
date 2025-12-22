a6c2b8b4c74a82334a9089fa7f6a17765701e108
Yumare1234 <yumaregabriel@gmail.com>
2025-12-22 19:33:41 -0400

    2 cartas nuevas y una barra de busqueda con el titulo

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 import Cartadetalle from './components/CartaProyecto';
 
 function App() {
-
+  const [busqueda, setBusqueda] = useState("");
 
   const Gojo = {
     nombre: "Satoru Gojo",
@@ -34,44 +34,49 @@ function App() {
     imagen: '/imagenes/Yuji.jpg', 
     Clan: "Kamo"
   };
+  
+  const Megumi = { 
+    nombre: "Megumi Fushiguro",
+    categoria: "Primer Grado",
+    ritual: "Técnicas de las 10 Sombras",
+    ataque: 5540,
+    defensa: 3530,
+    imagen: '/imagenes/Megumi.jpg', 
+    Clan: "Fushiguro / Zenin"
+  };
+
+  const Yuta = { 
+    nombre: "Yuta Okkotsu",
+    categoria: "Grado Especial",
+    ritual: "Mimetismo / Shikigami Rika Orimoto",
+    ataque: 7340,
+    defensa: 6730,
+    imagen: '/imagenes/Yuta.jpg', 
+    Clan: "Okkotsu"
+  };
+
 
   return (
-  <div className='py-12 bg-purple-700 justify-center'> Hola 
-    <div className="flex px-5 items-start flex-wrap min-h-screen bg-gray-100 bg-linear-to-br from-purple-900 via-purple-600 to-violet-900 gap-8">
-
-
-    <Cartadetalle
-    nombre={Gojo.nombre}
-    categoria={Gojo.categoria}
-    imagen={Gojo.imagen}
-    ritual={Gojo.ritual}
-    ataque={Gojo.ataque}
-    defensa={Gojo.defensa}
-    Clan={Gojo.Clan}
-    />
-
-<Cartadetalle
-    nombre={Sukuna.nombre}
-    categoria={Sukuna.categoria}
-    imagen={Sukuna.imagen}
-    ritual={Sukuna.ritual}
-    ataque={Sukuna.ataque}
-    defensa={Sukuna.defensa}
-    Clan={Sukuna.Clan}
-    />
-
-<Cartadetalle
-    nombre={Itadori.nombre}
-    categoria={Itadori.categoria}
-    imagen={Itadori.imagen}
-    ritual={Itadori.ritual}
-    ataque={Itadori.ataque}
-    defensa={Itadori.defensa}
-    Clan={Itadori.Clan}
-    />
-
+  <div className="flex flex-col min-h-screen bg-linear-to-br from-purple-900 via-purple-600 to-violet-900 p-5">
+    
+    <div className="flex flex-col md:flex-row items-center justify-center gap-6 mb-12 w-full">
+      <h1 className="text-4xl font-bold text-white">Juego de Cartas Estilo Anime</h1>
+      <input 
+        type="text"
+        placeholder="Buscar mazo o personaje..."
+        className="p-2 rounded-lg bg-purple-900/50 border border-purple-400 text-white outline-none w-64"
+      />
     </div>
+
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
+      <Cartadetalle nombre={Gojo.nombre} categoria={Gojo.categoria} imagen={Gojo.imagen} ritual={Gojo.ritual} ataque={Gojo.ataque} defensa={Gojo.defensa} Clan={Gojo.Clan} />
+      <Cartadetalle nombre={Sukuna.nombre} categoria={Sukuna.categoria} imagen={Sukuna.imagen} ritual={Sukuna.ritual} ataque={Sukuna.ataque} defensa={Sukuna.defensa} Clan={Sukuna.Clan} />
+      <Cartadetalle nombre={Itadori.nombre} categoria={Itadori.categoria} imagen={Itadori.imagen} ritual={Itadori.ritual} ataque={Itadori.ataque} defensa={Itadori.defensa} Clan={Itadori.Clan} />
+      <Cartadetalle nombre={Megumi.nombre} categoria={Megumi.categoria} imagen={Megumi.imagen} ritual={Megumi.ritual} ataque={Megumi.ataque} defensa={Megumi.defensa} Clan={Megumi.Clan} />
+      <Cartadetalle nombre={Yuta.nombre} categoria={Yuta.categoria} imagen={Yuta.imagen} ritual={Yuta.ritual} ataque={Yuta.ataque} defensa={Yuta.defensa } Clan = {Yuta.Clan } />
     </div>
+
+  </div>
   );
 }
 
